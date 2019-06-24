e6b2924b996b12e3c379d10b6b815e92824c091e
sirichai meemana <sirichai@o-kaatplus.com>
2019-06-24 22:57:10 +0700

    update function edit

--- resources/views/customer/show_order.blade.php
+++ resources/views/customer/show_order.blade.php
@@ -113,6 +113,46 @@
         </div>
     </div>
     <!-- End Modal add store-->
+
+    <!-- Modal add store-->
+    <div class="modal fade" id="edit-store" role="dialog">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header" style="background-color: #9BA2AB;">
+                    <h4 class="modal-title" style="color: #bbbfc3;">{!! trans('messages.bill.title') !!}</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="form">
+                                {!! Form::model(null,array('url' => array('customer/bill/add'),'class'=>'form-horizontal create-store-form','id'=>'form_add','method'=>'post','enctype'=>'multipart/form-data')) !!}
+                                <div class="form-group row">
+                                    <lable class="col-sm-4 control-label">{!! trans('messages.bill.title') !!}</lable>
+                                    <div class="col-sm-8">
+                                        {!! Form::file('photo',null,array('class'=>'form-control','placeholder'=>trans('messages.bill.photo'))) !!}
+                                        <input type="hidden" name="id_bill" class="id_order">
+                                    </div>
+                                </div>
+                                <div class="form-group row float-center" style="text-align: center; ">
+                                    <div class="col-sm-12">
+                                        <button class="btn-info btn-primary" id="add-store-btn" type="submit">Save</button>
+                                        {{--<button class="btn-info btn-warning" type="reset">Reset</button>--}}
+                                    </div>
+                                </div>
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- End Modal add store-->
 @endsection
 
 @section('script')
@@ -143,7 +183,8 @@
                     dataType : 'json',
                     data : {'id':id},
                     success : function(e){
-                        console.log(e);
+                       // console.log(e);
+                        $('#edit-store').modal('show');
                     } ,error : function(){
                         console.log('Error Search Data Store');
                     }
