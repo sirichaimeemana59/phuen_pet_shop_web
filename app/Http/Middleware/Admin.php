af89bd3113a098933edbb60cd590a94eaf86c679
sirichai <sirichai@o-kaatplus.com>
2019-06-27 17:19:35 +0700

    update function middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class Admin
      */
     public function handle($request, Closure $next)
     {
-        if(  Auth::guest() || (Auth::check() && Auth::user()->role !== 0 && Auth::user()->role !== 1)) {
+        if(  Auth::guest() || (Auth::check() && Auth::user()->role !== 0 && Auth::user()->role !== 0)) {
             return redirect('/');
         } else return $next($request);
     }
