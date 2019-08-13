fed94f3042de855ef80b851e505c11a53fec8bfa
sirichai <sirichai@o-kaatplus.com>
2019-08-13 12:49:12 +0700

    update page index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -94,7 +94,7 @@
                     }
                     Session::put('logo',$logo);
                 ?>
-                <a class="navbar-brand logo_h" href="index.html"><img src="{!! asset($logo) !!}" alt=""></a>
+                <a class="navbar-brand logo_h" href=""><img src="{!! asset($logo) !!}" alt=""></a>
                 {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">--}}
                     {{--<span class="icon-bar"></span>--}}
                     {{--<span class="icon-bar"></span>--}}
